a50badb9dc314bbc3c83f2b77e2024d5f74384d1
Jonas Bucher <j.bucher.mn@gmail.com>
2021-10-24 12:59:55 +0200

    Fix

--- newm-sidecar.sh
+++ newm-sidecar.sh
@@ -2,10 +2,9 @@
 ADDR=$(ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
 SCRIPTDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# newm-cmd open-virtual-output virt-1
-# trap 'newm-cmd close-virtual-output virt-1' EXIT
+newm-cmd open-virtual-output virt-1
+trap 'newm-cmd close-virtual-output virt-1' EXIT
 
-# Guacamole
 echo "Guacamole QR code"
 URL=$(python3 $SCRIPTDIR/newm_sidecar.py)
 URL=${URL/localhost/$ADDR}
