290058e2ed019d10f857d7b67124d23b47fdc18d
Jonas Bucher <j.bucher.mn@gmail.com>
2021-10-31 20:07:51 +0100

    Confirm prompt

--- newm-sidecar.sh
+++ newm-sidecar.sh
@@ -2,10 +2,15 @@
 ADDR=$(ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
 SCRIPTDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-newm-cmd open-virtual-output virt-1
-trap 'newm-cmd close-virtual-output virt-1' EXIT
-
 URL=$(python3 $SCRIPTDIR/newm_sidecar.py)
 URL=${URL/localhost/$ADDR}
 qrencode -m 2 -t utf8 <<< "$URL"
+
+if  [ "$1" != "-y" ]; then
+    read -p "Continue? [Y/n]" confirm && [[ $confirm == [yY] || -z $confirm ]] || exit 1
+fi
+
+newm-cmd open-virtual-output virt-1
+trap 'newm-cmd close-virtual-output virt-1' EXIT
+
 wayvnc --output=virt-1 --max-fps=30 localhost 5900
